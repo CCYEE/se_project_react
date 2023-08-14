37473b1e0f36ffeab9895fd482a95da1e4eb6c7c
CCYEE <ccyee4@gmail.com>
2023-08-13 23:29:54 -0700

    fixed api

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -19,10 +19,7 @@ export const apiKey = "9966b3564a314e82d57a5d0accfbc3eb";
 export const latitude = 44.34;
 export const longitude = 10.99;
 export const ESC_KEYCODE = 27;
-export const baseUrl =
-  process.env.NODE_ENV === "production"
-    ? "https://api.mefekki.moonangel.com"
-    : "http://localhost:3001";
+export const baseUrl = "https://api.mefekki.moonangel.com";
 export const weatherOptions = [
   {
     url: sunnyDay,
